5a49557268004841c0883e4750051ce83989cabc
Saymon <mdsymon703@gmail.com>
2023-10-04 13:20:10 +0600

    module4

--- assignment.php
+++ assignment.php
@@ -1,2 +1,28 @@
 <?php
-echo "hello would";
+
+class Product {
+    private $id;
+    private $name;
+    private $price;
+
+    public function __construct($id, $name, $price) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+
+    public function getFormattedPrice($price) {
+        return '$' . number_format($price, 2);
+    }
+
+    public function showDetails() {
+        echo "Product Details:\n";
+        echo "- ID: {$this->id}\n";
+        echo "- Name: {$this->name}\n";
+        echo "- Price: {$this->getFormattedPrice($this->price)}\n";
+    }
+}
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
